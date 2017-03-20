073fa4c02735590c740c6570d67687f048e1c627
Marco204391 <marco-frenc@hotmail.it>
2017-03-20 10:21:27 +0100

    3

--- src/it/polito/tdp/libretto_model/Libretto_model.java
+++ src/it/polito/tdp/libretto_model/Libretto_model.java
@@ -1,5 +1,21 @@
 package it.polito.tdp.libretto_model;
 
-public class Libretto_model {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Libretto_model {
+	
+	private List<Esame> esami;
+	
+	public Libretto_model(){
+		this.esami=  new ArrayList<Esame>();
+	}
+
+	/**
+	 * Aggiunge un nuove esame all'elenco degli esami presenti
+	 * @param e
+	 */
+	public void addEsame(Esame e){
+		esami.add(e);
+	}
 }
